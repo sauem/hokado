af4fea105754d463ac620a228a75fc33e80e90a6
sauem <nguyendinhthang.go97@gmail.com>
2021-03-02 01:34:15 +0700

    souce base

---
+++ common/config/main.php
@@ -0,0 +1,48 @@
+<?php
+return [
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm' => '@vendor/npm-asset',
+    ],
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'timeZone' => 'Asia/Ho_Chi_Minh',
+    'modules' => [
+        'rbac' => [
+            'class' => 'mdm\admin\Module',
+            'layout' => 'left-menu'
+        ],
+        'settings' => [
+            'class' => 'yii2mod\settings\Module',
+        ],
+    ],
+    'components' => [
+        'settings' => [
+            'class' => 'yii2mod\settings\components\Settings',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => SQL_HOST,
+            'username' => SQL_USER_NAME,
+            'password' => SQL_PASSWORD,
+            'charset' => 'utf8',
+            'enableSchemaCache' => true,
+            // Duration of schema cache.
+            'schemaCacheDuration' => 3600,
+            // Name of the cache component used to store schema information
+            'schemaCache' => 'cache',
+            'queryCache' => 'cache',
+            'enableQueryCache' => true,
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+    ],
+];
